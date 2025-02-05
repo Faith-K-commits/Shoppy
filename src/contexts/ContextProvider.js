de0100a0e547656cf3038f3c36be9e9b80131a1e
Faith Kariuki <tcwamboi@gmail.com>
2025-02-05 21:39:35 +0300

    Add navigation bar

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notifications: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  return (
+    // Values passed here are applied to entire application ie sidebar open or closed.
+    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
