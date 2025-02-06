9d2322d8de9794ecb36875cf86b15738affe8da4
Faith Kariuki <tcwamboi@gmail.com>
2025-02-06 19:55:18 +0300

    Make the Navbar responsive to small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
-    <TooltipComponent content='title' position='BottomCenter'>
+    <TooltipComponent content={title} position='BottomCenter'>
         <button className="relative text-xl rounded-full p-3 hover:bg-light-gray" type="button" onClick={customFunc} style={{color}}>
             <span style={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">{icon}</span>
         </button>
@@ -19,8 +19,28 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 )
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
-    const handleClick = () => {};
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+    useEffect(() => {
+        const handleResize = () =>
+            setScreenSize(window.innerWidth);
+            window.addEventListener('resize', handleResize)
+
+            handleResize();
+
+            return () => {
+                window.removeEventListener('resize', handleResize)
+            }
+        
+    }, []);
+    useEffect(() => {
+        if(screenSize <=900 ) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
+
     return (
         <div className='flex justify-between p-2 md:mx-6 relative'>
             <NavButton title="Menu" customFunc={() => {
@@ -30,10 +50,10 @@ const Navbar = () => {
                 <NavButton title="Cart" customFunc={() =>
                     handleClick('cart')
                 } color="blue" icon={<FiShoppingCart/>} />
-                <NavButton title="Chart" dotColor="#03C9D7" customFunc={() =>
-                    handleClick('chart')
+                <NavButton title="Chat" dotColor="#03C9D7" customFunc={() =>
+                    handleClick('chat')
                 } color="blue" icon={<BsChatLeft/>} />
-                <NavButton title="Notifications" customFunc={() =>
+                <NavButton title="Notification" customFunc={() =>
                     handleClick('notification')
                 } color="blue" icon={<RiNotification3Line/>} />
                 <TooltipComponent content="Profile" position="BottomCenter">
@@ -46,6 +66,10 @@ const Navbar = () => {
                     </div>
                     <MdKeyboardArrowDown className="text-gray-400 text-14" />
                 </TooltipComponent>
+                {isClicked.cart && <Cart />}
+                {isClicked.chat && <Chat />}
+                {isClicked.notification && <Notification />}
+                {isClicked.userProfile && <UserProfile />}
             </div>
         </div>
     )
